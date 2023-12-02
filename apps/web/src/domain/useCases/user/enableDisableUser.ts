d5f2e408e80b2f50a60e95a84d32687cfff54f1b
Daniel K. Guolo <daniel_kv@hotmail.com>
2023-12-02 14:12:04 -0300

    [web] add temp ts-expect-error

--- apps/web/src/domain/useCases/user/enableDisableUser.ts
+++ apps/web/src/domain/useCases/user/enableDisableUser.ts
@@ -1,5 +1,6 @@
 type TAction = 'enable' | 'disable'
 
+//@ts-expect-error
 export async function enableDisableUserUseCase(uid: string, action: TAction) {
     throw new Error('Não é possível habilitar ou desabilitar um usuário no momento')
 }
--- apps/web/src/domain/useCases/user/updateUser.ts
+++ apps/web/src/domain/useCases/user/updateUser.ts
@@ -1,5 +1,6 @@
 import { IUser } from 'oitoselo-models'
 
+//@ts-expect-error
 export async function updateUserUseCase(uid: string, data: Partial<IUser>): Promise<void> {
     throw new Error('Não é possível alterar um usuário no momento')
 }
